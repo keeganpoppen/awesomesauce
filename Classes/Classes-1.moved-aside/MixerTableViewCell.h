62de58706d30c4eee378d2774ff99652d6e39282
Ravi Parikh <rparikh@stanford.edu>
2011-02-14 03:27:00 -0800

    got rid of the stupid table

---
+++ Classes/Classes-1.moved-aside/MixerTableViewCell.h
@@ -0,0 +1,16 @@
+//
+//  MixerTableViewCell.h
+//  awesomesauce
+//
+//  Created by Ravi Parikh on 2/14/11.
+//  Copyright 2011 AwesomeBox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface MixerTableViewCell : UITableViewCell {
+
+}
+
+@end
