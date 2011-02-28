49b7e323705c7fa4c86f03168cfecdfce0765a85
Ravi Parikh <rparikh@stanford.edu>
2011-02-27 21:02:36 -0800

    synth and arrage views can go back to main view

---
+++ Classes/ArrangeViewController.mm
@@ -0,0 +1,43 @@
+    //
+//  ArrangeViewController.m
+//  awesomesauce
+//
+//  Created by Ravi Parikh on 2/27/11.
+//  Copyright 2011 AwesomeBox. All rights reserved.
+//
+
+#import "ArrangeViewController.h"
+
+
+@implementation ArrangeViewController
+@synthesize delegate;
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+	return (interfaceOrientation == UIInterfaceOrientationLandscapeRight);
+}
+
+- (IBAction) returnToMain:(id)sender {
+	[delegate closeArrangeView];
+}
+
+- (void)didReceiveMemoryWarning {
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc. that aren't in use.
+}
+
+- (void)viewDidUnload {
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+
+- (void)dealloc {
+    [super dealloc];
+}
+
+
+@end
