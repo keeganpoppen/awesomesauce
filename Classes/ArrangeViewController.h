a9035bff0f66382bd4c78128f6af1b6c3d5dcd29
Ravi Parikh <rparikh@stanford.edu>
2011-03-07 06:52:48 -0800

    deleted picker view stuff from arrange view

--- Classes/ArrangeViewController.h
+++ Classes/ArrangeViewController.h
@@ -18,36 +18,14 @@
 	IBOutlet UISlider *bpmSlider;
 	IBOutlet UITableView *tracks;
 	IBOutlet ArrangeTableViewCell *tblCell;
-	
-	IBOutlet UIPickerView *sharedTracks;
-	IBOutlet UITextField *trackName;
-	NSDictionary *currentlySharedTracks;
-	NSMutableArray *sharedTrackList;
 }
 
-@property (nonatomic, retain) NSDictionary *currentlySharedTracks;
-@property (nonatomic, retain) NSMutableArray *sharedTrackList;
 @property (nonatomic, retain) IBOutlet UISlider *bpmSlider;
 @property (nonatomic, retain) IBOutlet UITableView *tracks;
-@property (nonatomic, retain) IBOutlet UIPickerView *sharedTracks;
-@property (nonatomic, retain) IBOutlet UITextField *trackName;
-
 @property (nonatomic, retain) id <ArrangeViewProtocol, FlipViewProtocol> delegate;
 
 - (IBAction)returnToMain:(id)sender;
 - (IBAction)bpmChanged:(UISlider *)sender;
-- (IBAction)shareTrack:(id)sender;
-- (IBAction)loadTrack:(id)sender;
-
-//pickerview junk
-	//data source
-- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView;
-- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component;
-	//delegate
-- (CGFloat)pickerView:(UIPickerView *)pickerView rowHeightForComponent:(NSInteger)component;
-- (CGFloat)pickerView:(UIPickerView *)pickerView widthForComponent:(NSInteger)component;
-- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component;
-
 
 @end
 
